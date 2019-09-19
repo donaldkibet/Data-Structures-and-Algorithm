3601c14153875b1df8223d64164cf4e30415ccbd
Donald Kibet <chelashawdonald@yahoo.com>
2019-09-19 23:35:26 +0300

    Adding bubble sort algorithm

---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,33 @@
+namespace DataStructures.Algorithms
+{
+    public class BubbleSort
+    {
+        int tempNumber;
+        bool isSorted;
+        public void SortArray(int[] numbers)
+        {
+            if (numbers.Length > 0)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    isSorted = false;
+                    for (int i = 0; i < numbers.Length - 1; i++)
+                    {
+                        if (numbers[i] > numbers[i + 1])
+                        {
+                            // swap numbers
+                            tempNumber = numbers[i];
+                            numbers[i] = numbers[i + 1];
+                            numbers[i + 1] = tempNumber;
+                            isSorted = true;
+                        }
+                    }
+                    if (!isSorted)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataStructures.Algorithms;
 
 namespace DataStructures
 {
@@ -6,7 +7,15 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bubble sort the following numbers 10,5,6,9,14,2");
+            int[] numberArray = { 10, 5, 6, 9, 14, 2 };
+            var Bubble = new BubbleSort();
+            Bubble.SortArray(numberArray);
+            foreach (int number in numberArray)
+            {
+                Console.Write($" { number }");
+            }
+            Console.WriteLine("");
         }
     }
 }
