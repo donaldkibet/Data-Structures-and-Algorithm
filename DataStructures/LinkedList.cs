3eb69ecbf2e2b13fb68dd7ca3e34f0d8f36ecd05
Donald Kibet <chelashawdonald@yahoo.com>
2019-09-23 09:37:46 +0300

    Linked List Singly node definition

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,18 @@
+namespace DataStructures
+{
+    // Singly linked list Generic
+    public class Node<T>
+    {
+        public T nodeData;
+        public Node<T> next;
+        public Node(T data)
+        {
+            nodeData = data;
+            next = null;
+        }
+    }
+    public class LinkedList
+    {
+        
+    }
+}
